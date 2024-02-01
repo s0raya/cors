324dc48598f41c7a9473fc62228b44b7d1dcc311
s0raya <soraya.alcobet@gmail.com>
2024-02-01 20:06:27 +0100

    modified

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,37 @@
-function getCharacter() {
-    const characterInput = document.getElementById('characterName');
-    const characterInfo = document.getElementById('characterInfo');
+const characterInput = document.getElementById('characterName');
+const characterInfo = document.getElementById('characterInfo');
 
+function getCharacters() {
+    fetch('http://localhost:3000/characters')
+        .then(response => {
+            if(!response.ok) {
+                throw new Error('La solicitud no se puede procesar');
+            }
+            return response.json()
+        })
+        .then(data => {
+            characterInfo.innerHTML = '';
+            data.characters.forEach(element => {
+                characterInfo.innerHTML += `
+                <figure>
+                    <img src="${element.image}" alt="${element.name}">
+                    <figcaption>
+                        <h2>${element.name}</h2>
+                        <p>Status: ${element.status}</p>
+                        <p>Specie: ${element.species}</p>
+                        <p>Gender: ${element.gender}</p>
+                        <p>Origin: ${element.originName}</p>
+                    </figcaption>
+                </figure>
+                `
+            });
+        })
+        .catch(err => characterInfo.innerHTML += `<p>No se pueden obtener los datos, ${err}</p>`);
+}
+
+getCharacters();
+
+function getCharacter() {
     const characterName = characterInput.value.toLocaleLowerCase();
 
     fetch(`http://localhost:3000/characters/${characterName}`)
@@ -12,14 +42,19 @@ function getCharacter() {
             return response.json()
         })
         .then(data => {
+            characterInfo.innerHTML = '';
             const {name, status, species, gender, origin: {name: originName}, image} = data;
             characterInfo.innerHTML = `
-            <h2>${name}</h2>
-            <img src="${image}" alt="${name}">
-            <p>Status: ${status}</p>
-            <p>Specie: ${species}</p>
-            <p>Gender: ${gender}</p>
-            <p>Origin: ${originName}</p>
+            <figure>
+                <img src="${image}" alt="${name}">
+                <figcaption>
+                    <h2>${name}</h2>
+                    <p>Status: ${status}</p>
+                    <p>Specie: ${species}</p>
+                    <p>Gender: ${gender}</p>
+                    <p>Origin: ${originName}</p>
+                </figcaption>
+            </figure>
             `
         })
         .catch(err => characterInfo.innerHTML= `${err}: Imposible acceder al personaje`)
